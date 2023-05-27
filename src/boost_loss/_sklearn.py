49fa59a921ce3157f2fb0e09b03618fd2b151e7e
34j <55338215+34j@users.noreply.github.com>
2023-05-27 15:14:23 +0900

    feat: add TransformedTargetRegressor

--- src/boost_loss/_sklearn.py
+++ src/boost_loss/_sklearn.py
@@ -1,9 +1,11 @@
-from typing import TypeVar
+from typing import Any, TypeVar
 
 import catboost as cb
 import lightgbm as lgb
 import xgboost as xgb
-from sklearn.base import clone
+from sklearn.base import BaseEstimator, clone
+from sklearn.compose import TransformedTargetRegressor
+from sklearn.preprocessing import StandardScaler
 
 from ._base import LossBase
 
@@ -11,14 +13,20 @@ TEstimator = TypeVar("TEstimator", cb.CatBoost, lgb.LGBMModel, xgb.XGBModel)
 
 
 def apply_custom_loss(
-    estimator: TEstimator, loss: LossBase, *, copy: bool = False
-) -> TEstimator:
+    estimator: TEstimator,
+    loss: LossBase,
+    *,
+    copy: bool = True,
+    target_transformer: BaseEstimator | Any | None = StandardScaler()
+) -> TEstimator | TransformedTargetRegressor:
     if copy:
         estimator = clone(estimator)
     if isinstance(estimator, cb.CatBoost):
         estimator.set_params(loss_function=loss, eval_metric=loss)
     if isinstance(estimator, lgb.LGBMModel):
-        estimator.set_params(objective=loss, eval_metric=loss.eval_metric_lgb)
+        estimator.set_params(objective=loss, metric=[loss.eval_metric_lgb])
     if isinstance(estimator, xgb.XGBModel):
         estimator.set_params(objective=loss, eval_metric=loss.eval_metric_xgb_sklearn)
-    return estimator
+    if target_transformer is None:
+        return estimator
+    return TransformedTargetRegressor(estimator, transformer=clone(target_transformer))
