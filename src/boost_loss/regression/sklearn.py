59bf43010ffb552e1ce2c4f4458c2f2ac92f7e44
34j <55338215+34j@users.noreply.github.com>
2023-05-27 12:23:05 +0900

    feat(regression/sklearn): add predict and predict_var

--- src/boost_loss/regression/sklearn.py
+++ src/boost_loss/regression/sklearn.py
@@ -2,14 +2,15 @@ from typing import Any, Literal, Sequence
 
 import numpy as np
 from joblib import Parallel
-from sklearn.base import clone
+from numpy.typing import NDArray
+from sklearn.base import BaseEstimator, clone
 
 from .._base import LossBase
 from .._sklearn import apply_custom_loss
 from .asymmetric import AsymmetricLoss
 
 
-class VarianceEstimator:
+class VarianceEstimator(BaseEstimator):
     """Estimator that estimates the distribution by simply using multiple estimators
     with different `t`.
     Compared to [NGBoost](https://stanfordmlgroup.github.io/projects/ngboost/) or
@@ -29,6 +30,8 @@ class VarianceEstimator:
     """
 
     ts_: Sequence[float]
+    m_type: Literal["mean", "median"]
+    var_type: Literal["var", "std", "range", "mae", "mse"]
 
     def __init__(
         self,
@@ -38,7 +41,8 @@ class VarianceEstimator:
         ts: int | Sequence[float],
         n_jobs: int | None = 1,
         verbose: int = 0,
-        m_type: Literal["mean", "median"] = "mean",
+        random_state: int | None = None,
+        m_type: Literal["mean", "median"] = "median",
         var_type: Literal["var", "std", "range", "mae", "mse"] = "var",
     ) -> None:
         if not hasattr(estimator, "fit"):
@@ -50,6 +54,10 @@ class VarianceEstimator:
         self.ts = ts
         self.n_jobs = n_jobs
         self.verbose = verbose
+        self.m_type = m_type
+        self.var_type = var_type
+        self.random_state = random_state
+        self.random = np.random.RandomState(random_state)
 
     def fit(self, X: Any, y: Any) -> None:
         ts = self.ts
@@ -60,6 +68,10 @@ class VarianceEstimator:
             apply_custom_loss(clone(self.estimator), AsymmetricLoss(self.loss, t=t))
             for t in self.ts_
         ]
+        estimators_ = [
+            estimator.set_params(random_state=self.random.randint(2**32))
+            for estimator in estimators_
+        ]
         parallel_result = Parallel(n_jobs=self.n_jobs, verbose=self.verbose)(
             [estimator.fit(X, y) for estimator in estimators_]
         )
@@ -67,5 +79,52 @@ class VarianceEstimator:
             raise RuntimeError("joblib.Parallel returned None")
         self.estimators_ = estimators_
 
-    def predict_raw(self, X: Any) -> np.ndarray:
-        return np.array([estimator.predict(X) for estimator in self.estimators_])
+    def predict_raw(self, X: Any, **predict_params: Any) -> NDArray[Any]:
+        return np.array(
+            [estimator.predict(X, **predict_params) for estimator in self.estimators_]
+        )
+
+    def predict(
+        self,
+        X: Any,
+        type_: Literal["mean", "median", "var", "std", "range", "mae", "mse"]
+        | None = None,
+        **predict_params: Any,
+    ) -> NDArray[Any]:
+        type_ = type_ or self.m_type
+        if type_ == "mean":
+            return self.predict_raw(X, **predict_params).mean(axis=0)
+        elif type_ == "median":
+            return np.median(self.predict_raw(X, **predict_params), axis=0)
+        elif type_ == "var":
+            return self.predict_raw(X, **predict_params).var(axis=0)
+        elif type_ == "std":
+            return self.predict_raw(X, **predict_params).std(axis=0)
+        elif type_ == "range":
+            return self.predict_raw(X, **predict_params).max(axis=0) - self.predict_raw(
+                X, **predict_params
+            ).min(axis=0)
+        elif type_ == "mae":
+            return np.abs(
+                self.predict_raw(X, **predict_params)
+                - self.predict_raw(X, **predict_params).mean(axis=0)
+            ).mean(axis=0)
+        elif type_ == "mse":
+            return (
+                (
+                    self.predict_raw(X, **predict_params)
+                    - self.predict_raw(X, **predict_params).mean(axis=0)
+                )
+                ** 2
+            ).mean(axis=0)
+        else:
+            raise ValueError(f"Unknown type_: {type_}")
+
+    def predict_var(
+        self,
+        X: Any,
+        type_: Literal["var", "std", "range", "mae", "mse"] | None = None,
+        **predict_params: Any,
+    ) -> NDArray[Any]:
+        type_ = type_ or self.var_type
+        return self.predict(X, type_=type_, **predict_params)
